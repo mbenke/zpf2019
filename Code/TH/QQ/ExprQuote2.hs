4bbd68b6778ce5db7770951287862df4c38d3442
Marcin Benke <marcin.benke@gmail.com>
2019-04-19 16:05:58 +0200

    Code/TH

---
+++ Code/TH/QQ/ExprQuote2.hs
@@ -0,0 +1,42 @@
+-- Based on http://www.haskell.org/haskellwiki/Quasiquotation
+module ExprQuote2 where
+
+import Data.Generics.Aliases
+import qualified Language.Haskell.TH as TH
+import Language.Haskell.TH.Quote
+
+import Expr2
+
+expr  :: QuasiQuoter
+expr  =  QuasiQuoter 
+  { quoteExp = quoteExprExp
+  , quotePat = quoteExprPat
+  , quoteDec = undefined
+  , quoteType = undefined
+  }
+
+quoteExprExp s = do
+  pos <- getPosition
+  exp <- parseExp pos s
+  dataToExpQ (const Nothing  `extQ` antiExprExp) exp
+
+-- dataToExpQ :: Data a => (forall b. Data b => b -> Maybe (Q Exp)) -> a -> Q Exp
+
+quoteExprPat s = do
+  pos <- getPosition
+  exp <- parseExp pos s
+  dataToPatQ (const Nothing `extQ` antiExprPat) exp
+
+antiExprPat :: Exp -> Maybe (TH.Q TH.Pat)
+antiExprPat (EMetaVar v) = Just $ TH.varP (TH.mkName v)
+antiExprPat _ = Nothing
+
+antiExprExp :: Exp -> Maybe (TH.Q TH.Exp)
+antiExprExp (EMetaVar v) = Just $ TH.varE (TH.mkName v)
+antiExprExp _ = Nothing
+
+getPosition = fmap transPos TH.location where
+  transPos loc = (TH.loc_filename loc,
+                  fst (TH.loc_start loc),
+                  snd (TH.loc_start loc))
+                 
---
+++ Code/TH/QQ/TestExprQuote2.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE  QuasiQuotes #-}
+import ExprQuote2
+import Expr2
+
+t1 :: Exp
+t1 = [expr|1+2*3|]
+
+f1 :: Exp -> String
+f1 [expr| $a + $b |] = "Bingo!"
+f1 _ = "Sorry, no bonus" 
+
+eval [expr| $a + $b|] = eval a + eval b
+eval [expr| $a * $b|] = eval a * eval b
+eval (EInt n) = n
+
+test = eval [expr| 2+2 |]
+
+twice :: Exp -> Exp
+twice e = [expr| $e + $e |]
+
+testTwice = twice [expr| 3 * 3|]
+
+main = do
+     print test
+     print testTwice 
