4a824ea6d69423414d8b9bb22aca2225ff8b48c9
Marcin Benke <marcin.benke@gmail.com>
2019-05-02 11:55:15 +0200

    ExprQuote1 - liftData

--- Code/TH/QQ/ExprQuote1.hs
+++ Code/TH/QQ/ExprQuote1.hs
@@ -4,11 +4,11 @@ module ExprQuote1 where
 -- import Data.Generics
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
-
+import Language.Haskell.TH.Syntax
 import Expr
 
 expr  :: QuasiQuoter
-expr  =  QuasiQuoter 
+expr  =  QuasiQuoter
   { quoteExp = quoteExprExp
   , quotePat = quoteExprPat
   , quoteDec = undefined
@@ -18,9 +18,10 @@ expr  =  QuasiQuoter
 quoteExprExp s = do
   pos <- getPosition
   exp <- parseExp pos s
-  dataToExpQ (const Nothing) exp
+
+  dataToExpQ (const Nothing) exp -- equivalently `liftData exp`
 -- dataToExpQ :: Data a => (forall b. Data b => b -> Maybe (Q Exp)) -> a -> Q Exp
-  
+
 quoteExprPat :: String -> TH.Q TH.Pat
 quoteExprPat s = do
   pos <- getPosition
@@ -31,4 +32,3 @@ getPosition = fmap transPos TH.location where
   transPos loc = (TH.loc_filename loc,
                   fst (TH.loc_start loc),
                   snd (TH.loc_start loc))
-                 
