1087eef99831cc6951012c439173491fb72be407
Marcin Benke <marcin.benke@gmail.com>
2019-05-20 11:21:01 +0200

    fix tabs

--- Code/Concur/IORef2.hs
+++ Code/Concur/IORef2.hs
@@ -3,8 +3,8 @@ import Control.Concurrent
 
 incRef :: IORef Int -> IO ()
 incRef var = do { val <- readIORef var
-                ; threadDelay 1000         
-       	        ; writeIORef var (val+1) }
+                ; threadDelay 1000
+                ; writeIORef var (val+1) }
 
 main = do
   px <- newIORef 0
--- Code/Concur/IORef3.hs
+++ Code/Concur/IORef3.hs
@@ -4,8 +4,8 @@ import Control.Applicative((<$>),(<*),(*>))
 
 incRef :: IORef Int -> IO ()
 incRef var = do { val <- readIORef var
-                ; threadDelay 1000         
-       	        ; writeIORef var (val+1) }
+                ; threadDelay 1000
+                ; writeIORef var (val+1) }
 
 lock :: MVar () -> IO ()
 lock = takeMVar
@@ -15,15 +15,15 @@ unlock = flip putMVar ()
 
 locking :: IO a -> MVar () -> IO a
 action `locking` l = lock l >> (action <* unlock l)
-  
+
 main = do
   gil <- newMVar ()
   let atomically a = a `locking` gil
   main2 atomically
-          
+
 main2 atomically = do
   px <- newIORef 0
-  forkIO $ atomically $ incRef px 
-  forkIO $ atomically $ incRef px 
+  forkIO $ atomically $ incRef px
+  forkIO $ atomically $ incRef px
   threadDelay 3000
   readIORef px >>= print
